57195b1c834adf454c74878ec10ca014ca9b903f
CamilodeAssis <camilo.kotecki10@gmail.com>
2022-08-25 14:27:22 -0300

    importando base

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+
+const App= () =>{
+
+  return (
+    <div className="">
+      <header className='max-w-4xl my-10 mx-auto'>
+        <div>
+          <div className='p-2 w-28 flex align-center  justify-center text-5xl font-bold bg-sky-600 text-white'>IMC</div>
+        </div>
+      </header>
+      <div className='max-w-4xl flex m-auto '>
+        <div className='flex-1 mr-10'>
+          lef
+          </div>
+        <div className='flex-1 ml-10'>
+          right
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
